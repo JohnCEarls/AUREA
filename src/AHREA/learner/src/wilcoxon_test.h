8033be0d58b11d0342764aaed3521b0482518c30
John C. Earls <yt.rabb@gmail.com>
2011-02-18 15:53:11 -0600

    CC again

--- src/AHREA/learner/src/wilcoxon_test.h
+++ src/AHREA/learner/src/wilcoxon_test.h
@@ -1,10 +1,10 @@
 #ifndef WILCOXON_TEST
 #define WILCOXON_TEST
 #include <vector>
-extern "C"{
+//extern "C"{
 
 void wilcoxon_test(std::vector<double>& data,int numGenes, 
 int size_c1, int size_c2, std::vector<double> & scores);
-}
+//}
 #endif
 
