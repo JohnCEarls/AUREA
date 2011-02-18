4e5b0b5bd4b0cf9f114768c2ce7f3af11114e3d1
John C. Earls <yt.rabb@gmail.com>
2011-02-18 12:52:03 -0600

    More cleanup in setup

--- setup.py
+++ setup.py
@@ -2,14 +2,49 @@
 
 """
 setup.py 
-
-python setup.py build_ext --inplace
 """
 
 from distutils.core import setup, Extension
 import os
 import os.path
 import shutil
+
+def getAHREApyModules():
+    """
+    Returns a list of all py_modules in AHREA
+    """
+    #defining the modules
+    GUI_mod = [ 'AHREA.GUI.'+x for x in ['AHREAApp', 'AHREAController', 'AHREAPage', 'AHREAResults']] 
+    heuristic_mod = ['AHREA.heuristic.'+x for x in ['LearnerQueue', 'ResourceEstimate']]
+    packager_mod = ['AHREA.packager.'+x for x in ['DataCleaner','DataPackager']]
+    parser_mod = ['AHREA.parser.' + x for x in  ['CSVParser', 'GMTParser', 'SettingsParser', 'SOFTParser', 'SynonymParser']]
+    learner_mod = ["AHREA.learner.dirac", "AHREA.learner.tsp","AHREA.learner.tst", "AHREA.learner.ktsp", "AHREA.learner.wilcoxon"]
+    #putting all of the python modules in oneList
+    a_modules = []
+    for mod_list in [GUI_mod, heuristic_mod, packager_mod, parser_mod, learner_mod]:
+        for mod in mod_list:
+            a_modules.append(mod)
+    return a_modules
+
+def getExtensionModuleSettings():
+    """
+    Returns a list of tuples with the settings for the extension modules    
+    """
+    ext_mod_settings = []
+    ext_mod_settings.append(('AHREA.learner._dirac', ['Dirac_pywrapper.i', 'Dirac_pywrapper.cpp', 'Dirac.cpp', 'dir_Matrix.cpp','kfold.cpp' ]))
+    ext_mod_settings.append(('AHREA.learner._tsp',[ 'TSP_pywrapper.i', 'TSP_pywrapper.cpp', 'learn_utsp_classifier.cpp', 'utils.cpp', 'matrix.cpp', 'wilcoxon.cpp', 'order.cpp', 'kfold.cpp' ]))
+    ext_mod_settings.append(('AHREA.learner._tst',['TST_pywrapper.i', 'TST_pywrapper.cpp', 'learn_tst_classifier.cpp', 'utils.cpp', 'matrix.cpp', 'wilcoxon.cpp', 'order.cpp', 'kfold.cpp' ]))
+    ext_mod_settings.append(('AHREA.learner._ktsp',['KTSP_pywrapper.i', 'KTSP_pywrapper.cpp', 'Ktsp.cpp', 'kfold.cpp',  'utils.cpp', 'matrix.cpp', 'wilcoxon.cpp', 'order.cpp']))
+    ext_mod_settings.append(('AHREA.learner._wilcoxon',['WILCOXON_pywrapper.i', 'WILCOXON_pywrapper.cpp', 'wilcoxon_test.cpp', 'utils.cpp', 'matrix.cpp', 'wilcoxon.cpp', 'order.cpp' ]))
+    return ext_mod_settings
+
+def buildExtension(ext_name,src_dir, src_files, swig_opts=['-modern', '-c++']):
+    """
+    Builds and returns an extension module with the given settings
+    """
+    sources_list = [os.path.join(src_dir, src_file) for src_file in src_files]
+    return Extension(ext_name, sources=sources_list, swig_opts=swig_opts)
+
 
 def swig_helper( module, src_dir ):
     """
@@ -25,70 +60,39 @@ def swig_helper( module, src_dir ):
     
     shutil.copy(os.path.join(src_dir, module.lower() + ".py"),os.sep.join(['src', 'AHREA', 'learner' ] ))
 
-modules = ["Dirac", "TSP", "TST", "KTSP", "WILCOXON"]
+#start script
+
+learner_modules = ["Dirac", "TSP", "TST", "KTSP", "WILCOXON"]
 path_dir = ['src', 'AHREA', 'learner', 'src']
 src_dir = os.sep.join(path_dir)
 
-for mod in modules:
+#copy swig generated pymodules to the learner directory
+print "running swig_helper"
+for mod in learner_modules:
     swig_helper(mod,src_dir)
 
-#define modules
-
-dirac_module = Extension('AHREA.learner._dirac', 
-    sources=map(lambda x: os.path.join(src_dir, x), ['Dirac_pywrapper.i', 'Dirac_pywrapper.cpp', 'Dirac.cpp', 'dir_Matrix.cpp','kfold.cpp' ]), 
-
-    swig_opts=['-modern', '-c++'],
-                        )
-tsp_module = Extension('AHREA.learner._tsp',
-    sources=map(lambda x: os.path.join(src_dir, x),[ 'TSP_pywrapper.i', 'TSP_pywrapper.cpp', 'learn_utsp_classifier.cpp', 'utils.cpp', 'matrix.cpp', 'wilcoxon.cpp', 'order.cpp', 'kfold.cpp' ])
-,
-    swig_opts=['-modern', '-c++'],
-                    )     
-
-tst_module = Extension('AHREA.learner._tst',
-    sources = map(lambda x: os.path.join(src_dir, x),['TST_pywrapper.i', 'TST_pywrapper.cpp', 'learn_tst_classifier.cpp', 'utils.cpp', 'matrix.cpp', 'wilcoxon.cpp', 'order.cpp', 'kfold.cpp' ])
-,
-    swig_opts=['-modern', '-c++'],
-                        )
-
-ktsp_module = Extension('AHREA.learner._ktsp',
-    sources = map(lambda x: os.path.join(src_dir, x),['KTSP_pywrapper.i', 'KTSP_pywrapper.cpp', 'Ktsp.cpp', 'kfold.cpp',  'utils.cpp', 'matrix.cpp', 'wilcoxon.cpp', 'order.cpp'])
-,
-    swig_opts=['-modern', '-c++'],
-                    )
-wilcoxon_module = Extension('AHREA.learner._wilcoxon',
-    sources = map(lambda x: os.path.join(src_dir, x),['WILCOXON_pywrapper.i', 'WILCOXON_pywrapper.cpp', 'wilcoxon_test.cpp', 'utils.cpp', 'matrix.cpp', 'wilcoxon.cpp', 'order.cpp' ]),
-    swig_opts=['-modern', '-c++'],
-                    )
-
-#defining the modules
-GUI_mod = [ 'AHREA.GUI.'+x for x in ['AHREAApp', 'AHREAController', 'AHREAPage', 'AHREAResults']] 
-heuristic_mod = ['AHREA.heuristic.'+x for x in ['LearnerQueue', 'ResourceEstimate']]
-packager_mod = ['AHREA.packager.'+x for x in ['DataCleaner','DataPackager']]
-parser_mod = ['AHREA.parser.' + x for x in  ['CSVParser', 'GMTParser', 'SettingsParser', 'SOFTParser', 'SynonymParser']]
-learner_mod = ["AHREA.learner.dirac", "AHREA.learner.tsp","AHREA.learner.tst", "AHREA.learner.ktsp", "AHREA.learner.wilcoxon"]
-#putting all of the python modules in oneList
-a_modules = []
-for mod_list in [GUI_mod, heuristic_mod, packager_mod, parser_mod, learner_mod]:
-    for mod in mod_list:
-        a_modules.append(mod)
-
+#Build extension instances
+ext_mod = [ buildExtension(name, src_dir, src_files) for name, src_files in getExtensionModuleSettings()]
 
 #run setup
 setup (name = 'AHREA: Adaptive Heuristic Relative Expression Analyser',
         version='1.0',
         author ='John C. Earls',
+        author_email = 'earls3@illinois.edu',
+        url= 'https://github.com/JohnCEarls/AHREAPackage.git',
         description="""AHREA is a sofware suite that makes finding Relative Expression Learners easy.""",
         packages=['AHREA','AHREA.GUI', 'AHREA.learner', 'AHREA.packager', 'AHREA.parser','AHREA.heuristic', 'pyBabel'],
         package_dir={'AHREA': 'src/AHREA', 'pyBabel': 'src/pyBabel'},
         scripts=['scripts/AHREAGUI.py'],
+        ext_modules = ext_mod, 
+        py_modules = getAHREApyModules(),
+        classifiers=[
+            'License :: OSI Approved :: GNU Affero General Public License v3', 
+            'Development Status :: 4 - Beta', 
+            'Intended Audience :: Science/Research', 
+            'Topic :: Scientific/Engineering :: Bio-Informatics', 
+            'Topic :: Scientific/Engineering :: Medical Science Apps.'],
 
-        
-
-        #ext_package='learner',
-        ext_modules = [dirac_module, tsp_module, tst_module, ktsp_module, wilcoxon_module],
-        py_modules = a_modules,
-
-        )
+)
 
 
