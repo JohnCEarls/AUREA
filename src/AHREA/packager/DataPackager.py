43c69098d2ca8aff44d4640869a4356e4420f727
John C. Earls <yt.rabb@gmail.com>
2011-02-21 17:31:54 -0600

    Bad import

--- src/AHREA/packager/DataPackager.py
+++ src/AHREA/packager/DataPackager.py
@@ -370,7 +370,7 @@ This builds the geneNet Vector from the provided information.  It uses gene syno
         """
 Adds a table of synonyms to allow cross referencing between geneNets and datasets.
         """
-        import parser.SynonymParser
+        import AHREA.parser.SynonymParser
         self.synonyms = parser.SynonymParser.SynonymParser()
         self.synonyms.importgene_info(file)
 
