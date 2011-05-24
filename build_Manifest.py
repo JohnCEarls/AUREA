81ba552ff343dbcb4ae91871dba273c13c30264c
John C. Earls <yt.rabb@gmail.com>
2011-05-24 17:34:00 -0500

    Updated Manifest
    Manifest has workspace in it now.

--- build_Manifest.py
+++ build_Manifest.py
@@ -11,4 +11,7 @@ print "setup.py"
 for root, _ , files in os.walk('src'):
     for file in files:
         print os.path.join(root,file)
+for root, _ , files in os.walk('workspace'):
+    for file in files:
+        print os.path.join(root,file)
 
