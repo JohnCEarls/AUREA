5138bbad5a4c786655c655780c3ef2739dc6fd26
John C. Earls <yt.rabb@gmail.com>
2011-05-24 18:19:20 -0500

    Manifest again
    pointing the build_Manifest.py at MANIFEST.in instead of MANIFEST

--- build_Manifest.py
+++ build_Manifest.py
@@ -1,18 +1,20 @@
 """
-Creates a proper MANIFEST
+Creates a proper MANIFEST template
 
 run with:
 
-python build_Manifest.py > MANIFEST
+python build_Manifest.py > MANIFEST.in
 """
 import os
-print "README"
-print "setup.py"
-print "ez_setup.py"
+print "include README"
+print "include setup.py"
+print "include ez_setup.py"
 for root, _ , files in os.walk('src'):
     for file in files:
+        print "include ",
         print os.path.join(root,file)
 for root, _ , files in os.walk('workspace'):
     for file in files:
+        print "include ",
         print os.path.join(root,file)
 
