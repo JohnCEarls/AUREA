66867172b9a292a94946207d60a601a4c2cbcc44
John C. Earls <yt.rabb@gmail.com>
2011-05-24 18:08:13 -0500

    Added ez_setup to Manifest

--- build_Manifest.py
+++ build_Manifest.py
@@ -8,6 +8,7 @@ python build_Manifest.py > MANIFEST
 import os
 print "README"
 print "setup.py"
+print "ez_setup.py"
 for root, _ , files in os.walk('src'):
     for file in files:
         print os.path.join(root,file)
