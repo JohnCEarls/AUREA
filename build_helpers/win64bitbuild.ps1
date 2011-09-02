f2f0610ff41b3ab728708b0838003cd3a371b7c5
unknown <earls3@.(none)>
2011-09-02 17:39:22 -0500

    Updated version on win64bitbuild.ps1

--- build_helpers/win64bitbuild.ps1
+++ build_helpers/win64bitbuild.ps1
@@ -19,35 +19,10 @@ Building ...
 python setup.py bdist_egg
 python setup.py bdist_wininst
 python setup.py bdist
-cp C:\Users\earls3\AUREA\dist\AUREA-1.6.2.win-amd64.zip C:\Users\earls3\AUREA\dist\AUREA-1.6.2.win-amd64-py2.$v.zip 
-rm C:\Users\earls3\AUREA\dist\AUREA-1.6.2.win-amd64.zip
+cp C:\Users\earls3\AUREA\dist\AUREA-1.6.3.win-amd64.zip C:\Users\earls3\AUREA\dist\AUREA-1.6.3.win-amd64-py2.$v.zip 
+rm C:\Users\earls3\AUREA\dist\AUREA-1.6.3.win-amd64.zip
 }
 $env:Path = $origPath
 $env:Path
 
 cp C:\Users\earls3\AUREA\dist\* C:\Users\earls3\Dropbox\Price\builds
-
-
-
-
-##creates abd sets up build for testing
-Get-ChildItem Env:
-$pyVs = @(7)
-$origPath = $env:Path
-foreach($v in $pyVs){
-
-$env:PYTHON_LIB = "C:\Python2"+$v+"\libs\Python2" + $v +".lib"
-$env:PYTHON_INCLUDE = "C:\Python2"+$v+"\include"
-$env:Path = $origPath + ";C:\Python2" + $v
-Get-ChildItem Env:
-$env:Path
-$oldpylib = $env:PYTHON_LIB
-
-$env:PYTHON_LIB = $null
-$test = $env:PYTHON_LIB
-"
-Building ...
-"
-python setup.py build
-}
-$env:PYTHONPATH = "C:\Users\earls3\AUREA\build\lib.win-amd64-2.7"
