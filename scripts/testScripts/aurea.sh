33b02c6d179456ca128a34c63676a3c107ff4fd0
Victor Cassen <phonybone@gmail.com>
2012-02-07 04:28:05 -0800

    transfer

--- scripts/testScripts/aurea.sh
+++ scripts/testScripts/aurea.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export PYTHONPATH=/mnt/price1/vcassen/AUREA/build/lib.linux-x86_64-2.7/:/mnt/price1/vcassen/trends/pylib/
+export PYTHONPATH=${AUREA_HOME}/build/lib.linux-x86_64-2.7/:${TRENDS_HOME}/pylib/
 
-AUREA_DIR=/mnt/price1/vcassen/AUREA/scripts/testScripts/
+AUREA_DIR=${AUREA_HOME}/scripts/testScripts/
 cd ${AUREA_DIR}
 
 python aurea.py -a normal.csv -b tumor.csv -l tsp -c config.xml
--- src/AUREA/packager/DataCleaner.py
+++ src/AUREA/packager/DataCleaner.py
@@ -15,8 +15,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import os.path
-import copy, re, yaml
-from GEO.Sample import Sample
+import copy, re, yaml, sys
+#from GEO.Sample import Sample
+print "\n".join(sys.path)
 from warn import *
 
 class DataTable:
