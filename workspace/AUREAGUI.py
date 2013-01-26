a82465b35a16497adb966403066c0863bbd0baa6
John C. Earls <yt.rabb@gmail.com>
2013-01-25 20:20:13 -0800

    Windows exe stuff

--- workspace/AUREAGUI.py
+++ workspace/AUREAGUI.py
@@ -6,6 +6,19 @@ import os
 import tkFileDialog
 from tkMessageBox import *
 import re
+import platform
+
+import imp, os, sys
+
+def main_is_frozen():
+   return (hasattr(sys, "frozen") or # new py2exe
+           hasattr(sys, "importers") # old py2exe
+           or imp.is_frozen("__main__")) # tools/freeze
+
+def get_main_dir():
+   if main_is_frozen():
+       return os.path.dirname(sys.executable)
+   return os.path.dirname(sys.argv[0])
 
 def newWorkspace(root):
     """
@@ -27,7 +40,25 @@ def newWorkspace(root):
 root = Tkinter.Tk()
 init_workspace =  os.getcwd()
 
-if re.compile('Application').search(init_workspace):
+if platform.system() == 'Windows' and main_is_frozen():
+    #py2exe standalone
+    appdata = os.getenv('APPDATA')
+    if appdata is not None:
+        workspace = os.path.join(appdata, 'AUREA')
+        
+        if not os.path.exists(workspace):
+            os.mkdir(workspace)
+        if not os.path.exists(os.path.join(workspace, 'data')):
+            os.mkdir(os.path.join(workspace, 'data'))
+        #copy files
+        needful_things = ['config.xml', 'AUREA-logo-200.pgm', 'Homo_sapiens.gene_info.gz','c2.biocarta.v2.5.symbols.gmt' ]
+        for thing in needful_things:
+            if not os.path.exists( os.path.join(workspace, 'data', thing)):
+                shutil.copy(os.path.join(old_workspace, 'data',thing), os.path.join(workspace, 'data'))
+        os.chdir(workspace)
+
+
+elif platform.system() == 'Darwin' and re.compile(r'AUREA\.app').search(init_workspace):
     #this is a py2app build
     #move workspace out of the application folder
     import shutil
